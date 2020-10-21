b2149b4e548f89e3900df806caf0ccc354a6bcd3
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-10-21 23:01:00 +0200

    import images from webpack

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@ import {example} from './js/example'
 // Import of styles
 import './styles/index.scss'
 
+// Importing images
+import illustrationBoxDesktop from './images/illustration-box-desktop.svg'
+import illustrationWomanOnlineDesktop from './images/illustration-woman-online-desktop.svg'
+
+// Appending to the DOM
+const box = document.createElement('img')
+box.src = illustrationBoxDesktop
+
+const womanOnline = document.createElement('img')
+womanOnline.src = illustrationWomanOnlineDesktop
+
+const boxDiv = document.querySelector('#box')
+const womanOnlineDiv = document.querySelector('#woman-online')
+
+boxDiv.append(box)
+womanOnlineDiv.append(womanOnline)
+
+// Init JS Functions
 example();
