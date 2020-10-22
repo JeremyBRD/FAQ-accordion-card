af957a16f8fb78d1a1ea31c93cbc495cebd02f9a
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-10-22 11:43:21 +0200

    add image bg pattern

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import './styles/index.scss'
 // Importing images
 import illustrationBoxDesktop from './images/illustration-box-desktop.svg'
 import illustrationWomanOnlineDesktop from './images/illustration-woman-online-desktop.svg'
+import bgPatternDesktop from './images/bg-pattern-desktop.svg'
 
 // Appending to the DOM
 const box = document.createElement('img')
@@ -15,11 +16,16 @@ box.src = illustrationBoxDesktop
 const womanOnline = document.createElement('img')
 womanOnline.src = illustrationWomanOnlineDesktop
 
+const bgPattern = document.createElement('img')
+bgPattern.src = bgPatternDesktop
+
 const boxDiv = document.querySelector('#box')
 const womanOnlineDiv = document.querySelector('#woman-online')
+const bgPatternDiv = document.querySelector('#bg-pattern')
 
 boxDiv.append(box)
 womanOnlineDiv.append(womanOnline)
+bgPatternDiv.append(bgPattern)
 
 // Init JS Functions
 example();
