955370b168e79b75f565b50d040b3c2717d2864e
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-10-30 15:47:26 +0100

    add toggle class active

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -2,7 +2,19 @@ const accordionItemHeaders = document.querySelectorAll('.accordion-item-header')
 
 const toggleActive = () => accordionItemHeaders.forEach(accordionItemHeader => {
   accordionItemHeader.addEventListener('click', event => {
-    accordionItemHeader.lastElementChild.classList.toggle('active');
+    const currentlyActiveArrow = document.querySelector('.arrow-down.active')
+    if (currentlyActiveArrow && currentlyActiveArrow !== accordionItemHeader) {
+      currentlyActiveArrow.classList.toggle('active');
+      currentlyActiveArrow.parentElement.nextElementSibling.style.maxHeight = 0;
+    } else {
+      accordionItemHeader.lastElementChild.classList.toggle('active');
+      const accordionBody = accordionItemHeader.nextElementSibling;
+      if (accordionItemHeader.lastElementChild.classList.contains('active')) {
+        accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
+      } else {
+        accordionBody.style.maxHeight = 0;
+      }
+    }
   })
 });
 
